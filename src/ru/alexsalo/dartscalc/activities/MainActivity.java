a88be816871bbf9787f725ac27ff079b68d4c08c
alexsalo <alexsalovrn@gmail.com>
2013-03-28 22:24:20 +0400

    cvs saving + some logic repaired

---
+++ src/ru/alexsalo/dartscalc/activities/MainActivity.java
@@ -0,0 +1,35 @@
+package ru.alexsalo.dartscalc.activities;
+
+import ru.alexsalo.dartscalc.R;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+	private Button classic_train501_btn;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		classic_train501_btn = (Button)findViewById(R.id.classic_train501);
+		
+		classic_train501_btn.setOnClickListener(new OnClickListener() {
+			public void onClick(View arg0) {
+				Intent i = new Intent(MainActivity.this, ClassicTrain501Activity.class);
+				startActivity(i);
+			}
+		});
+		return true;
+	}
+
+}
---
+++ src/ru/alexsalo/dartscalc/logic/xmlDataBuilder.java
@@ -0,0 +1,56 @@
+package ru.alexsalo.dartscalc.logic;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import android.os.Environment;
+import android.text.format.Time;
+import android.util.Log;
+
+public class xmlDataBuilder {
+	FileWriter writer;
+	File DartsCalcData;
+	File filecsv;
+	String game_mode;
+	Time time = new Time();
+
+	public xmlDataBuilder(String gameMode) {
+		game_mode = gameMode;
+	}
+
+	public String saveToXml(ArrayList<int[]> mas) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < mas.size(); i++)
+			sb.append(String.valueOf(mas.get(i)[0]) + ","
+					+ String.valueOf(mas.get(i)[1]) + ","
+					+ String.valueOf(mas.get(i)[2]) + ","
+					+ String.valueOf(mas.get(i)[3]) + ","
+					+ String.valueOf(mas.get(i)[4]) + "\r\n");
+		time.setToNow();
+		String filename = "training_" + time.format("%Y%m%d_%H%M%S") + ".csv";
+		String baseDir = Environment.getExternalStorageDirectory()
+				.getAbsolutePath();
+		if (Environment.getExternalStorageState().equals(
+				Environment.MEDIA_MOUNTED)) {
+			DartsCalcData = new File(baseDir + File.separator + "DartsCalcData"
+					+ File.separator + game_mode);
+			if (!DartsCalcData.exists())
+				DartsCalcData.mkdirs();
+			filecsv = new File(DartsCalcData.getAbsoluteFile(), filename);
+			try {
+				FileOutputStream os = new FileOutputStream(filecsv);
+				OutputStreamWriter out = new OutputStreamWriter(os);
+				out.write(sb.toString());
+				out.close();
+			} catch (IOException e) {
+				Log.w("ExternalStorage", "Error writing " + filecsv, e);
+			}
+
+		}
+
+		return sb.toString();
+	}
+}
