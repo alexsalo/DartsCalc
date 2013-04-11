c6a8255eff9cf5cdd073d30bbb27ef15b56bb72c
alexsalo <alexsalovrn@gmail.com>
2013-04-11 15:10:32 +0400

    achieve repaired

--- src/ru/alexsalo/dartscalc/logic/Achievements.java
+++ src/ru/alexsalo/dartscalc/logic/Achievements.java
@@ -3,20 +3,27 @@ package ru.alexsalo.dartscalc.logic;
 import java.util.HashMap;
 
 public class Achievements {
-	private String[] badges = new String[]{"Ничего", "3 Юношеский", "2 Юношеский", "1 юношеский",
-			"3 разряд", "2 разряд", "1 разряд", "КМС", "Мастер Спорта"};
+	private String[] badges = new String[] { "Ничего", "3 Юношеский",
+			"2 Юношеский", "1 юношеский", "3 разряд", "2 разряд", "1 разряд",
+			"КМС", "Мастер Спорта" };
 	private HashMap<GameModes, int[]> table = new HashMap<GameModes, int[]>();
-	
-	public Achievements(){
-		table.put(GameModes.BIG_ROUND, new int[]{0,250,320,390,420,550,660,750,900});
+
+	public Achievements() {
+		table.put(GameModes.BIG_ROUND, new int[] { -1, 180, 250, 250, 320, 320,
+				390, 360, 420, 450, 550, 580, 660, 650, 750, 810, 900 });
 	}
-	public String getAchievement(boolean male, GameModes gameMode, int score){
-		int i;
-		int [] scoretable = table.get(gameMode);
-		for (i = 0; i<=15; i++){
-			if (score < scoretable[i])
-				break;
-		}
-		return badges[i];
+
+	public String getAchievement(boolean male, GameModes gameMode, int score) {
+		int i=0;
+		int[] scoretable = table.get(gameMode);
+		if (male)
+			for (i = 0; i <= 16; i += 2) 
+				if (score < scoretable[i])
+					break;			
+		else 
+			for (i = 1; i <= 16; i += 2) 
+				if (score < scoretable[i])
+					break;
+		return male ? badges[(i / 2)-1] : badges[(i / 2)];
 	}
 }
